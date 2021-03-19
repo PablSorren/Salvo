3ca2c9cba23afae3a88c11dbc36857969d6df271
Pabl <psorrentinod@gmail.com>
2021-03-19 16:52:58 -0300

    Task 2 of the Salvo web game application version 1.0 *fix recursitivy with @JsonIgnore

--- src/main/java/com/codeoftheweb/salvo/SalvoController.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -13,17 +14,25 @@ import java.util.stream.Collectors;
 public class SalvoController {
 
      @Autowired
-    GamePlayerRepository gamePlayerRepository;
+    GameRepository gameRepository;
 
     @RequestMapping("/games")
-     public List<Object> getGamesId(){
-         return gamePlayerRepository
+     public List< Map<String, Object> > getGamesId(){
+         return gameRepository
                  .findAll()
                  .stream()
-                 .map(gamePlayer -> gamePlayer.getGame().getGameId())
+                 .map(game -> gamesDTO(game))
                  .collect(Collectors.toList());
      }
 
+
+     private Map<String, Object> gamesDTO(Game game){
+
+        Map<String, Object> dto = new LinkedHashMap<>();
+        dto.put("id", game.getGameId() );
+        dto.put("created", game.getDateAndTimeOfCreation());
+        return  dto;
+     }
 
 
 
