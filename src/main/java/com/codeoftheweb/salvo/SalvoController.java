04a22592399ff6728bf4474e1443b452e0c42815
Pabl <psorrentinod@gmail.com>
2021-03-23 11:04:46 -0300

    Task 2 of the Salvo web game application version 1.1 *finish version

--- src/main/java/com/codeoftheweb/salvo/SalvoController.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoController.java
@@ -29,7 +29,7 @@ public class SalvoController {
                 .collect(Collectors.toList());
     }
 
-
+    //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
     @GetMapping("/games")
      public List< Map<String, Object> > getGames(){
          return gameRepository
