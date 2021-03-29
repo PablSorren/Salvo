1f7cfb9f6db92d17d911a8e97a7e0fe36a583c9e
Pabl <psorrentinod@gmail.com>
2021-03-29 16:55:06 -0300

    Task 3 of the Salvo web game application
    *Added salvo examples in SalvoApplication

--- src/main/java/com/codeoftheweb/salvo/ShipType.java
+++ src/main/java/com/codeoftheweb/salvo/ShipType.java
@@ -1,3 +1,16 @@
 package com.codeoftheweb.salvo;
 
-public enum ShipType {Carrier, Battleship, Submarine, Destroyer,Patrol_Boat}
+public enum ShipType {
+
+    Carrier(5), Battleship(4), Submarine(3), Destroyer(3),Patrol_Boat(2) ;
+
+    int length;
+
+    ShipType(int length) {
+        this.length = length;
+    }
+
+    int getLength(){
+        return length;
+    }
+}
